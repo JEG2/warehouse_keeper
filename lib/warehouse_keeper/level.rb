a4f4fa40e6e07519f244fdb85b640169e43d44e1
James Edward Gray II <james@graysoftinc.com>
2013-11-30 15:55:41 -0600

    Rouding out level parsing.

--- lib/warehouse_keeper/level.rb
+++ lib/warehouse_keeper/level.rb
@@ -1,26 +1,71 @@
 module WarehouseKeeper
   class Level
+    class Void; end
     class Wall; end
+    class Floor
+      def initialize(contents = nil)
+        @contents = contents
+      end
+
+      attr_reader :contents
+    end
+    class Goal < Floor; end
+
     class Player; end
     class Gem; end
-    class Floor; end
-    class Goal; end
 
-    KEY = {
-      "#" => Wall,
-      "@" => Player,
-      "$" => Gem,
-      " " => Floor,
-      "." => Goal
+    PARSE_KEY = {
+      "-" => lambda { Void.new              },
+      "#" => lambda { Wall.new              },
+      " " => lambda { Floor.new             },
+      "." => lambda { Goal.new              },
+      "@" => lambda { Floor.new(Player.new) },
+      "$" => lambda { Floor.new(Gem.new)    }
     }
 
     def self.parse(string)
-      input = string.split("\n")
-      rows = input.map { |row| row.split(//).map { |cell|
-        KEY[cell].new
-        } }
-      new(rows)
+      # build square board
+      clean = string.gsub(/^ *(?:;.*)?\n/, "")
+      width = clean.lines.map { |row| row.rstrip.size }.max
+      rows  = clean.lines.map { |row| row.rstrip.ljust(width).chars }
+
+      # fill-in void
+      2.times do
+        rows.each do |row|
+          row.fill("-", 0, row.index("#"))
+          row.fill("-", row.rindex("#") + 1)
+        end
+        rows = rows.transpose
+      end
+
+      # construct level
+      new(
+        rows.map { |row|
+          row.map { |cell| PARSE_KEY[cell].call }
+        }
+      )
     end
+
+    def self.from_file( num, path = File.join( __dir__,
+                                               *%w[ ..
+                                                    ..
+                                                    levels
+                                                    original_and_extra.txt ] ) )
+      buffer = [ ]
+      File.foreach(path) do |line|
+        if line =~ /\A\s*;\s*(\d+)/
+          if $1.to_i == num
+            return parse(buffer.join)
+          else
+            buffer.clear
+          end
+        else
+          buffer << line
+        end
+      end
+    end
+
+    include Enumerable
 
     def initialize(rows)
       @rows = rows
@@ -28,6 +73,10 @@ module WarehouseKeeper
 
     def [](x,y)
       @rows[y][x]
+    end
+
+    def each(&block)
+      @rows.each(&block)
     end
   end
 end
--- spec/level_spec.rb
+++ spec/level_spec.rb
@@ -6,6 +6,21 @@ describe WarehouseKeeper::Level do
   let(:minimal) { WarehouseKeeper::Level.parse( "######\n" +
                                                 "#@$ .#\n" +
                                                 "######\n" ) }
+  let(:tricky) {
+    WarehouseKeeper::Level.parse(<<END_LEVEL)
+    #####
+    #   #
+    #$  #
+  ###  $##
+  #  $ $ #
+### # ## #   ######
+#   # ## #####  ..#
+# $  $          ..#
+##### ### #@##  ..#
+    #     #########
+    #######
+END_LEVEL
+  }
 
   it "parses walls" do
     minimal[0, 0].must_be_instance_of(WarehouseKeeper::Level::Wall)
@@ -19,4 +34,40 @@ describe WarehouseKeeper::Level do
     minimal[4, 1].must_be_instance_of(WarehouseKeeper::Level::Goal)
   end
 
+  it "parses the player" do
+    minimal[1, 1].must_be_instance_of(WarehouseKeeper::Level::Floor)
+    minimal[1, 1].contents.must_be_instance_of(WarehouseKeeper::Level::Player)
+  end
+
+  it "parses gems" do
+    minimal[2, 1].must_be_instance_of(WarehouseKeeper::Level::Floor)
+    minimal[2, 1].contents.must_be_instance_of(WarehouseKeeper::Level::Gem)
+  end
+
+  it "makes all levels square" do
+    tricky.map { |row| row.size }.uniq.size.must_equal(1)
+  end
+
+  it "ignores cells outside the board" do
+    tricky[10, 5].must_be_instance_of(WarehouseKeeper::Level::Void)
+    tricky[3, 5].must_be_instance_of(WarehouseKeeper::Level::Floor)
+  end
+
+  it "ignores comments and blank lines" do
+    noisy = WarehouseKeeper::Level.parse(<<END_LEVEL)
+; This is a comment
+
+######
+#@$ .#
+######
+; 1
+END_LEVEL
+    noisy.to_a.size.must_equal(3)
+  end
+
+  it "can parse a level from a file" do
+    level_2 = WarehouseKeeper::Level.from_file(2)
+    level_2[0, 0].must_be_instance_of(WarehouseKeeper::Level::Wall)
+    level_2[12, 0].must_be_instance_of(WarehouseKeeper::Level::Void)
+  end
 end
